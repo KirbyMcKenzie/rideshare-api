88c8d725c6361e746a5f86fdb76d1d588fa2096d
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2025-04-22 19:43:15 -0400

    Add RidesController action for POST /api/rides and supporting services
    - Add RidesController action for POST /api/rides for creating rides
    using the start_address and destination_address as body params.
    - Add RoutingService for geocoding and route calculation
    - Add RideCalculationService for pay calculations.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     resources :drivers, only: [] do
       get "rides", on: :member
     end
+
+    resources :rides, only: [ :create ]
   end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
